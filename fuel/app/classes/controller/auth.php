0234e0fff754106920e638cc6c28149ffcf466a0
Zero <john.royer@gmail.com>
2012-11-10 20:50:35 +0800

    Fix Indent and Adding Annotation
    Using tabs rather than space in indent. Adding annotation to method.

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -1,16 +1,14 @@
 <?php
-
 /**
- * This controller used to make user authenication.
- */
+ * Authenication. For user login, register, logout, etc.
+ **/
 class Controller_Auth extends Controller
 {
-   
-   /**
-    * Default page. Show login page and register form.
-    */
-   public function action_index()
-   {
+	/**
+	 * Default page. Show login and register form.
+	 **/
+	public function action_index()
+	{
 		$view = View::forge('user/home');
 		$data = array(
 			'header' => View::forge('header'),
@@ -18,7 +16,7 @@ class Controller_Auth extends Controller
 			'page_title' => 'aniTrace',
 			'loggedin' => false,
 		);
-		$view->set_global( $data );
+		$view->set_global($data);
 		return $view;
-   }
+	}
 }
