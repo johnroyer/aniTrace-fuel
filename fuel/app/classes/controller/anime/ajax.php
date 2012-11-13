ad5f22185ea048b1cb5a72630fbc35abc3a5d895
Zero <john.royer@gmail.com>
2012-11-13 14:22:03 +0800

    Add Controller for Ajax Requests

---
+++ fuel/app/classes/controller/anime/ajax.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Methods for Ajax request in animtion page.
+ **/
+class Controller_Anime_Ajax extends Controller_Anime
+{
+	/**
+	 * Default method. Return all animte availible for user.
+	 **/
+	public function action_index()
+	{
+		echo 'ajax';
+	}
+}
