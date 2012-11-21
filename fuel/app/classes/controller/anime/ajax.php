511d405c4418b195d3a236e80a9e447cccefb7b7
Zero <john.royer@gmail.com>
2012-11-21 14:36:37 +0800

    Add Volumn Modifier

--- fuel/app/classes/controller/anime/ajax.php
+++ fuel/app/classes/controller/anime/ajax.php
@@ -28,9 +28,34 @@ class Controller_Anime_Ajax extends Controller_Anime
 	 **/
 	public function action_anime($id=0)
 	{
-		if( intval($id) > 0 )
+		$id = intval($id);
+		if( $id > 0 )
 		{
 			echo json_encode(Anime::getAnime($id));
+		}
+	}
+
+	/**
+	 * Add or subtract anime volumn.
+	 * @param  string  'up' or 'down'
+	 * @param  int     anime ID
+	 * @return json    anime information after update
+	 **/
+	public function action_vol($action, $id=0)
+	{
+		$id = intval($id);
+		if( $id > 0 )
+		{
+			if( $action == 'up' )
+			{
+				Anime::volumnUp($id);
+				echo json_encode(Anime::getAnime($id));
+			}
+			else
+			{
+				Anime::volumnDown($id);
+				echo json_encode(Anime::getAnime($id));
+			}
 		}
 	}
 }
