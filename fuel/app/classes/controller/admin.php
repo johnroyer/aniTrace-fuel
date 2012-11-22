87c497fcb4967fe2048da312e0997a06878f84dc
Zero <john.royer@gmail.com>
2012-11-22 14:50:59 +0800

    Add Default Admin Controller

---
+++ fuel/app/classes/controller/admin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin panel home page.
+ **/
+class Controller_Admin extends Controller
+{
+	/**
+	 * Default action.
+	 **/
+	public function action_index()
+	{
+		return 'admin';
+	}
+}
