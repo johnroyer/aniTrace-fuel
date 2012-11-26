c675a694c8ae8becac93ef7a168cbbc40cf87973
Zero <john.royer@gmail.com>
2012-11-26 15:08:00 +0800

    Change Variable Name
    Change variable '$alert' to '$dialog'.

--- fuel/app/views/dialog.php
+++ fuel/app/views/dialog.php
@@ -1,35 +1,36 @@
-<?php  $this->load->view('header');  ?>
-<?php  $this->load->view('navbar');  ?>
-<?php  $this->load->helper('url');  ?>
+<?php
+   echo View::forge('header');
+   echo View::forge('navbar');
+?>
 
 <div class="in-center">
 
-   <?php if( !isset($alert['type']) ): ?>
+   <?php if( !isset($dialog['type']) ): ?>
       <div class="well">
-   <?php elseif( $alert['type'] == 'error' ): ?>
+   <?php elseif( $dialog['type'] == 'error' ): ?>
       <div class="alert alert-error">
-   <?php elseif( $alert['type'] == 'warning' ): ?>
+   <?php elseif( $dialog['type'] == 'warning' ): ?>
       <div class="alert">
-   <?php elseif( $alert['type'] == 'success' ): ?>
+   <?php elseif( $dialog['type'] == 'success' ): ?>
       <div class="alert alert-success">
-   <?php elseif( $alert['type'] == 'info' ): ?>
+   <?php elseif( $dialog['type'] == 'info' ): ?>
       <div class="alert alert-info">
    <?php else: ?>
       <div class="well">
    <?php endif; ?>
 
 
-   <?php if( isset($alert['title'] )): ?>
-      <h3><?php echo $alert['title']; ?></h3>
+   <?php if( isset($dialog['title'] )): ?>
+      <h3><?php echo $dialog['title']; ?></h3>
    <?php endif; ?>
 
-   <p><?php echo $alert['text']; ?></p>
+   <p><?php echo $dialog['text']; ?></p>
 
-   <?php if( isset( $alert['next'] ) ): ?>
-      <a class="btn" href="<?php echo $alert['next']; ?>"><?php echo $alert['next_hint']; ?></a>
+   <?php if( isset( $dialog['next'] ) ): ?>
+      <a class="btn" href="<?php echo $dialog['next']; ?>"><?php echo $dialog['next_hint']; ?></a>
    <?php endif; ?>
 
    </div>
 </div>
 
-<?php $this->load->view('footer');  ?>
+<?php echo View::forge('footer'); ?>
