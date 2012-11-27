1ea5fb88c83b1dd2dba7a082616833f846a21e41
Zero <john.royer@gmail.com>
2012-11-27 11:18:28 +0800

    Modify Delete Cofirm Page to Fit FuelPHP

--- fuel/app/views/admin/delete_confirm.php
+++ fuel/app/views/admin/delete_confirm.php
@@ -1,20 +1,21 @@
-<?php  $this->load->view('header');  ?>
-<?php  $this->load->view('navbar');  ?>
-<?php  $this->load->helper('url');  ?>
+<?php
+   echo View::forge('header');
+   echo View::forge('navbar');
+?>
 
-<h2>User Management</h2>
+<h2>使用者管理</h2>
 <p style="margin-top: 25px;"> </p>
 
 <div class="span6 pull-center alert alert-error">
-<h3>刪除確認</h3>
-<p>你正準備要刪除一個帳號！</p>
-<p>ID: <?php echo $target['id']; ?></p>
-<p>帳號： <?php echo $target['username']; ?></p>
-<p>Email: <?php echo $target['email']; ?></p>
-<p>
-   <a class="btn btn-danger" href="<?php echo site_url('admin/deleteUser/'.$target['id'].'/confirmed');?>">刪除</a>
-   <a class="btn" href="<?php echo site_url('admin/'); ?>">取消</a>
-</p>
+   <h3>刪除確認</h3>
+   <p>你正準備要刪除一個帳號！</p>
+   <p>ID: <?php echo $target['id']; ?></p>
+   <p>帳號： <?php echo $target['username']; ?></p>
+   <p>Email: <?php echo $target['email']; ?></p>
+   <p>
+      <a class="btn btn-danger" href="<?php echo Uri::create('admin/deleteUser/'.$target['id'].'/confirmed'); ?>">刪除</a>
+      <a class="btn" href="<?php echo Uri::create('admin/'); ?>">取消</a>
+   </p>
 </div>
 
-<?php $this->load->view('footer');  ?>
+<?php echo View::forge('footer'); ?>
