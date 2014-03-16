f6cfc3febfbafd2cbab32a1424e56fa166ad3cb2
Zero <john.royer@gmail.com>
2014-03-16 21:04:52 +0800

    Add Model
    ORM Model for table 'anime_lists'

---
+++ fuel/app/classes/model/anime/list.php
@@ -0,0 +1,27 @@
+<?php
+
+class Model_Test_List extends \Orm\Model
+{
+	protected static $_properties = array(
+		'id',
+		'user_id',
+		'name',
+		'sub',
+		'volumn',
+		'download',
+		'link',
+		'finished',
+		'public',
+		'updated',
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+			'property' => 'updated',
+		),
+	);
+	protected static $_table_name = 'test_lists';
+
+}
