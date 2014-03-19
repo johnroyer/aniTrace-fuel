d1f82b8730a0c39fa95c4bba1c0efd044ef2d02a
Zero <john.royer@gmail.com>
2014-03-20 00:44:48 +0800

    Set Column Nullable

--- fuel/app/migrations/003_add_updated_to_anime_lists.php
+++ fuel/app/migrations/003_add_updated_to_anime_lists.php
@@ -7,7 +7,7 @@ class Add_updated_to_anime_lists
 	public function up()
 	{
 		\DBUtil::add_fields('anime_lists', array(
-			'updated' => array('constraint' => 11, 'type' => 'int'),
+			'updated' => array('constraint' => 11, 'type' => 'int', 'null' => true),
 
 		));
 	}
