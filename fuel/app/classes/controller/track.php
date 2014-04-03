4af44e0dec1fbaf6cf0be04ce963b3b5eb5b8764
Zero <john.royer@gmail.com>
2014-04-03 22:05:31 +0800

    Add New Controller

---
+++ fuel/app/classes/controller/track.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ */
+class Controller_Track extends Controller
+{
+   /**
+    * Construtor for Anime
+    **/
+   public function before()
+   {
+      if(!Sentry::check())
+      {
+         Response::redirect(Uri::create('auth/'));
+      }
+   }
+}
