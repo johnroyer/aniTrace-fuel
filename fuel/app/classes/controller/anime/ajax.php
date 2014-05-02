f84beefadfe93f650b34ae2c16ec9e8538ea8752
Zero <john.royer@gmail.com>
2014-05-02 20:33:09 +0800

    No More Tabs

--- fuel/app/classes/controller/anime/ajax.php
+++ fuel/app/classes/controller/anime/ajax.php
@@ -7,114 +7,114 @@ use \Model\Anime_List;
  **/
 class Controller_Anime_Ajax extends Controller
 {
-	/**
-	 * Construtor for Anime
-	 **/
-	public function before()
-	{
-		if(!Sentry::check())
-		{
-			Response::redirect(Uri::create('auth/'));
-		}
-	}
+   /**
+    * Construtor for Anime
+    **/
+   public function before()
+   {
+      if(!Sentry::check())
+      {
+         Response::redirect(Uri::create('auth/'));
+      }
+   }
 
-	/**
-	 * Default method. Return all availible animes which is not finished user.
-	 **/
-	public function action_index()
-	{
-		return json_encode(Anime::getList('download'));
-	}
+   /**
+    * Default method. Return all availible animes which is not finished user.
+    **/
+   public function action_index()
+   {
+      return json_encode(Anime::getList('download'));
+   }
 
-	/**
-	 * Return all watchable animes.
-	 **/
-	public function action_watchableList()
-	{
-		return json_encode(Anime::getList('watchable'));
-	}
+   /**
+    * Return all watchable animes.
+    **/
+   public function action_watchableList()
+   {
+      return json_encode(Anime::getList('watchable'));
+   }
 
-	/**
-	 * Return anime information.
-	 * @param  int   anime ID
-	 * @return json  anime information, empty array if not found
-	 **/
-	public function action_anime($id=0)
-	{
-		$id = intval($id);
-		$anime = Model_Anime_List::find($id);
+   /**
+    * Return anime information.
+    * @param  int   anime ID
+    * @return json  anime information, empty array if not found
+    **/
+   public function action_anime($id=0)
+   {
+      $id = intval($id);
+      $anime = Model_Anime_List::find($id);
 
       if($anime == null){
-			return new Response($stat, 404);  // not found
+         return new Response($stat, 404);  // not found
       }
       return json_encode($anime->to_array());
-	}
+   }
 
-	/**
-	 * Add or subtract anime volumn.
-	 * @param  string  'up' or 'down'
-	 * @param  int     anime ID
-	 * @return json    anime information after update
-	 **/
-	public function action_vol($action, $id=0)
-	{
-		$id = intval($id);
-		$anime = Model_Anime_List::find($id);
+   /**
+    * Add or subtract anime volumn.
+    * @param  string  'up' or 'down'
+    * @param  int     anime ID
+    * @return json    anime information after update
+    **/
+   public function action_vol($action, $id=0)
+   {
+      $id = intval($id);
+      $anime = Model_Anime_List::find($id);
 
-		if($anime === null){
-			$stat = json_encode(array('stat' => 'item not found'));
-			return new Response($stat, 404);  // not found
-		}
+      if($anime === null){
+         $stat = json_encode(array('stat' => 'item not found'));
+         return new Response($stat, 404);  // not found
+      }
 
-		// Can only delete self's list
-		if($anime->user_id !== Sentry::user()->get('id')){
-			$stat = json_encode(array('stat' => 'access denied'));
-			return new Response($stat, 406);  // not accessable
-		}
+      // Can only delete self's list
+      if($anime->user_id !== Sentry::user()->get('id')){
+         $stat = json_encode(array('stat' => 'access denied'));
+         return new Response($stat, 406);  // not accessable
+      }
 
-		if($action == 'up'){
-			$anime->volumn++;
-			$anime->save();
-			return json_encode(Anime::getAnime($id));
-		}else{
-			$anime->volumn--;
-			$anime->save();
-			return json_encode(Anime::getAnime($id));
-		}
-	}
+      if($action == 'up'){
+         $anime->volumn++;
+         $anime->save();
+         return json_encode(Anime::getAnime($id));
+      }else{
+         $anime->volumn--;
+         $anime->save();
+         return json_encode(Anime::getAnime($id));
+      }
+   }
 
-	/**
-	 * Add or subtract anime download progress.
-	 * @param  string  'up' or 'down'
-	 * @param  int     anime ID
-	 * @return json    anime information after update
-	 **/
-	public function action_download($action, $id=0)
-	{
-		$id = intval($id);
-		$anime = Model_Anime_List::find($id);
+   /**
+    * Add or subtract anime download progress.
+    * @param  string  'up' or 'down'
+    * @param  int     anime ID
+    * @return json    anime information after update
+    **/
+   public function action_download($action, $id=0)
+   {
+      $id = intval($id);
+      $anime = Model_Anime_List::find($id);
 
-		if($anime === null){
-			$stat = json_encode(array('stat' => 'item not found'));
-			return new Response($stat, 404);  // not found
-		}
+      if($anime === null){
+         $stat = json_encode(array('stat' => 'item not found'));
+         return new Response($stat, 404);  // not found
+      }
 
-		// Can only delete self's list
-		if($anime->user_id !== Sentry::user()->get('id')){
-			$stat = json_encode(array('stat' => 'access denied'));
-			return new Response($stat, 406);  // not accessable
-		}
+      // Can only delete self's list
+      if($anime->user_id !== Sentry::user()->get('id')){
+         $stat = json_encode(array('stat' => 'access denied'));
+         return new Response($stat, 406);  // not accessable
+      }
 
-		if($action == 'up'){
-			$anime->download++;
-			$anime->save();
-			return json_encode(Anime::getAnime($id));
-		}else{
-			$anime->download--;
-			$anime->save();
-			return json_encode(Anime::getAnime($id));
-		}
-	}
+      if($action == 'up'){
+         $anime->download++;
+         $anime->save();
+         return json_encode(Anime::getAnime($id));
+      }else{
+         $anime->download--;
+         $anime->save();
+         return json_encode(Anime::getAnime($id));
+      }
+   }
 
    /**
     * Set if anime has finished.
@@ -143,38 +143,38 @@ class Controller_Anime_Ajax extends Controller
       return json_encode(Anime::getAnime($id));
    }
 
-	/**
-	 * Add a new anime.
-	 * @return  json  anime information which has been inserted
-	 **/
-	public function action_add()
-	{
-		$validate = Validation::forge();
-		$validate->add_field('name', 'anime name', 'required');
-		$validate->add_field('link', 'external link', 'max_length[10240]');
-		if( $validate->run() ){
-			$anime = new Model_Anime_List();
-			$anime->user_id = Sentry::user()->get('id');
-			$anime->name = Input::post('name');
-			$anime->sub = Input::post('sub', '');
-			//$anime->volumn = Input::post('volumn', 0);
-			//$anime->download = Input::post('download', 0);
-			//$anime->link = Input::post('link', '');
-			//$anime->finished = Input::post('finished', 0);
-			//$anime->public = Input::post('public', 0);
-			$anime->save();
-			return json_encode($anime->to_array());
-		}else{
-			echo json_encode(array('stat'=>'input error'));
-		}
-	}
+   /**
+    * Add a new anime.
+    * @return  json  anime information which has been inserted
+    **/
+   public function action_add()
+   {
+      $validate = Validation::forge();
+      $validate->add_field('name', 'anime name', 'required');
+      $validate->add_field('link', 'external link', 'max_length[10240]');
+      if( $validate->run() ){
+         $anime = new Model_Anime_List();
+         $anime->user_id = Sentry::user()->get('id');
+         $anime->name = Input::post('name');
+         $anime->sub = Input::post('sub', '');
+         //$anime->volumn = Input::post('volumn', 0);
+         //$anime->download = Input::post('download', 0);
+         //$anime->link = Input::post('link', '');
+         //$anime->finished = Input::post('finished', 0);
+         //$anime->public = Input::post('public', 0);
+         $anime->save();
+         return json_encode($anime->to_array());
+      }else{
+         echo json_encode(array('stat'=>'input error'));
+      }
+   }
 
-	/**
-	 * Modify anime information
-	 * @return  json  anime information after update
-	 **/
-	public function action_mod()
-	{
+   /**
+    * Modify anime information
+    * @return  json  anime information after update
+    **/
+   public function action_mod()
+   {
       $id = intval(Input::post('id', 0));
       $anime = Model_Anime_List::find($id);
 
@@ -196,42 +196,42 @@ class Controller_Anime_Ajax extends Controller
       $anime->link = Input::post('link', '');
       $anime->save();
       return json_encode($anime->to_array());
-	}
+   }
 
    public function action_delete($id=0) {
       $id = intval($id);
-		$user = $this->getUserInfo();
-		$anime = Model_Anime_List::find($id);
+      $user = $this->getUserInfo();
+      $anime = Model_Anime_List::find($id);
 
-		// find item
-		if($anime === null){
-			return json_encode(array('stat' => 'error'));
-		}
+      // find item
+      if($anime === null){
+         return json_encode(array('stat' => 'error'));
+      }
 
-		// Can only delete self's list
-		if($anime->user_id !== Sentry::user()->get('id')){
-			return json_encode(array('stat' => 'error'));
-		}
+      // Can only delete self's list
+      if($anime->user_id !== Sentry::user()->get('id')){
+         return json_encode(array('stat' => 'error'));
+      }
 
-		// delete it
-		try{
-			$anime->delete();
-			return json_encode(array('stat'=>'ok'));
-		}catch(Exception $e){
-			return json_encode(array('stat' => 'error'));
-		}
+      // delete it
+      try{
+         $anime->delete();
+         return json_encode(array('stat'=>'ok'));
+      }catch(Exception $e){
+         return json_encode(array('stat' => 'error'));
+      }
    }
 
-	/**
-	 * Return user information needed in views.
-	 **/
-	private function getUserInfo()
-	{
-		$user = Sentry::user();
-		$result = array(
-			'username' => $user->get('username'),
-			'isAdmin' => true,
-		);
-		return $result;
-	}
+   /**
+    * Return user information needed in views.
+    **/
+   private function getUserInfo()
+   {
+      $user = Sentry::user();
+      $result = array(
+         'username' => $user->get('username'),
+         'isAdmin' => true,
+      );
+      return $result;
+   }
 }
--- fuel/app/migrations/003_add_updated_to_anime_lists.php
+++ fuel/app/migrations/003_add_updated_to_anime_lists.php
@@ -4,19 +4,19 @@ namespace Fuel\Migrations;
 
 class Add_updated_to_anime_lists
 {
-	public function up()
-	{
-		\DBUtil::add_fields('anime_lists', array(
-			'updated' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+   public function up()
+   {
+      \DBUtil::add_fields('anime_lists', array(
+         'updated' => array('constraint' => 11, 'type' => 'int', 'null' => true),
 
-		));
-	}
+      ));
+   }
 
-	public function down()
-	{
-		\DBUtil::drop_fields('anime_lists', array(
-			'updated'
+   public function down()
+   {
+      \DBUtil::drop_fields('anime_lists', array(
+         'updated'
 
-		));
-	}
+      ));
+   }
 }
