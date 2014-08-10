ebc342bff0d9a9a0db5184f883fd5abe20eee167
Zero <john.royer@gmail.com>
2014-08-10 20:53:20 +0800

    Add API Controller

---
+++ fuel/app/classes/apijson.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class ApiJson extends Controller_Rest {
+	// always use JSON as output format
+   protected $format = 'json';
+
+   // rewrite routing rule here
+   public function router($resouce, $arguments){
+      parent::router($resouce, $arguments);
+   }
+}
--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -4,4 +4,7 @@ return array(
 	'_404_'   => 'home/404',    // The main 404 route
 	
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+
+	// API controller routing
+	'api/v1/(:alpha)/(:any)' => 'api/v1/$1/$1/$2',
 );
