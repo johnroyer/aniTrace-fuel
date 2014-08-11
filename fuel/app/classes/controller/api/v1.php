cc108072f72a1092b7d65f06af5ba1ac3d73fb09
Zero <john.royer@gmail.com>
2014-08-11 22:14:42 +0800

    Add Anime List

--- fuel/app/classes/controller/api/v1.php
+++ fuel/app/classes/controller/api/v1.php
@@ -1,7 +1,21 @@
 <?php
 
+use \Model\Anime;
+use \Model\Anime_list;
+
 class Controller_Api_V1 extends ApiJson {
+   private function auth(){
+      if(!Sentry::check()){
+         Response::redirect(Uri::create('auth'));
+      }
+   }
+
    public function get_track(){
-      return array('hello' => 'world');
+      $this->auth();
+
+      $list = Anime::getList('watchable');
+      //var_dump($list);
+      //return $list;
+      echo json_encode($list);
    }
 }
