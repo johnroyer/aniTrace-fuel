31c08485408e83198df3c123cbfa25a61e24894e
Zero <john.royer@gmail.com>
2015-03-04 21:24:19 +0800

    Add RoutingFunction for API Classes

--- fuel/app/classes/apijson.php
+++ fuel/app/classes/apijson.php
@@ -2,5 +2,10 @@
 
 abstract class ApiJson extends Controller_Rest {
 	// always use JSON as output format
-	protected $format = 'json';
+   protected $format = 'json';
+
+   // rewrite routing rule here
+   public function router($resouce, $arguments){
+      parent::router($resouce, $arguments);
+   }
 }
