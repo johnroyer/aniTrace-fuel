9a46ebf5fd23fe38a7e599a1e751d58f92462206
Zero <john.royer@gmail.com>
2015-04-25 23:22:12 +0800

    Fix Coding Style

--- fuel/app/classes/controller/api/v1/track.php
+++ fuel/app/classes/controller/api/v1/track.php
@@ -3,38 +3,42 @@
 use \Model\Anime;
 use \Model\Track;
 
-class Controller_Api_V1_Track extends ApiJson {
-   /**
-    * Get specific track
-    *
-    * @param int $id track ID
-    */
-   public function get_track($id){
-      if($id == 'all'){
-         return $this->response(Anime::getList('watchable'));
-      }else{
-         $id = intval($id);
-         $anime = Model_Track::find($id);
+class controller_api_v1_track extends ApiJson
+{
+    /**
+     * Get specific track
+     *
+     * @param int $id track ID
+     */
+    public function get_track($id)
+    {
+        if ($id == 'all') {
+            return $this->response(Anime::getList('watchable'));
+        } else {
+            $id = intval($id);
+            $anime = Model_Track::find($id);
 
-         if($anime === null){
-            return $this->response('track not found', 404);
-         }else{
-            return $this->response($anime->to_array());
-         }
-      }
-   }
+            if ($anime === null) {
+                return $this->response('track not found', 404);
+            } else {
+                return $this->response($anime->to_array());
+            }
+        }
+    }
 
-   /**
-    * Update track infomation
-    *
-    * @param int $id track ID
-    */
-   public function put_track($id){
-   }
+    /**
+     * Update track infomation
+     *
+     * @param int $id track ID
+     */
+    public function put_track($id)
+    {
+    }
 
-   /**
-    * Create new track
-    */
-   public function post_track(){
-   }
+    /**
+     * Create new track
+     */
+    public function post_track()
+    {
+    }
 }
