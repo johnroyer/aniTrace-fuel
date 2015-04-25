86dc327d04150c2a67c9ab1d754471b6ff86c710
Zero <john.royer@gmail.com>
2015-04-25 23:00:53 +0800

    Add Comments

--- fuel/app/classes/controller/api/v1/track.php
+++ fuel/app/classes/controller/api/v1/track.php
@@ -4,6 +4,11 @@ use \Model\Anime;
 use \Model\Track;
 
 class Controller_Api_V1_Track extends ApiJson {
+   /**
+    * Get specific track
+    *
+    * @param int $id track ID
+    */
    public function get_track($id){
       if($id == 'all'){
          return $this->response(Anime::getList('watchable'));
@@ -19,9 +24,17 @@ class Controller_Api_V1_Track extends ApiJson {
       }
    }
 
+   /**
+    * Update track infomation
+    *
+    * @param int $id track ID
+    */
    public function put_track($id){
    }
 
-   public function post_track($id){
+   /**
+    * Create new track
+    */
+   public function post_track(){
    }
 }
