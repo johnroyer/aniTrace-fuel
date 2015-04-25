a53885c1ccde5931415d0872d3b4d1b9a26d0734
Zero <john.royer@gmail.com>
2015-04-25 14:29:09 +0800

    Change Tabs to Spaces

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -2,33 +2,33 @@
 
 class Controller_Home extends Controller
 {
-	/**
-	 * Redirect if user loggedin.
-	 **/
-	public function before()
-	{
-		if( Sentry::check() )
-		{
-			Response::redirect(Uri::create('tracker/'));
-		}
-	}
+    /**
+     * Redirect if user loggedin.
+     **/
+    public function before()
+    {
+        if( Sentry::check() )
+        {
+            Response::redirect(Uri::create('tracker/'));
+        }
+    }
 
-	public function action_index()
-	{
-		$view = View::forge('home/index');
-		$data = array(
-			'header' => View::forge('header'),
-			'navbar' => View::forge('navbar'),
-			'page_title' => 'aniTrace',
-			'loggedin' => false,
-		);
-		$view->set_global( $data );
+    public function action_index()
+    {
+        $view = View::forge('home/index');
+        $data = array(
+            'header' => View::forge('header'),
+            'navbar' => View::forge('navbar'),
+            'page_title' => 'aniTrace',
+            'loggedin' => false,
+        );
+        $view->set_global( $data );
 
-		return $view;
-	}
+        return $view;
+    }
 
-	public function action_404()
-	{
-		return '404';
-	}
+    public function action_404()
+    {
+        return '404';
+    }
 }
--- fuel/app/classes/controller/install.php
+++ fuel/app/classes/controller/install.php
@@ -3,20 +3,20 @@
 class Controller_Install extends Controller
 {
 
-	public function action_index()
-	{
-		if( Migrate::latest('sentry', 'package') && Migrate::latest('default', 'app'))
-		{
-			return 'install complete';
-		}
-		else
-		{
-			return 'migrate failed';
-		}
-	}
+    public function action_index()
+    {
+        if( Migrate::latest('sentry', 'package') && Migrate::latest('default', 'app'))
+        {
+            return 'install complete';
+        }
+        else
+        {
+            return 'migrate failed';
+        }
+    }
 
-	public function action_version()
-	{
-		return Migrate::current('default');
-	}
+    public function action_version()
+    {
+        return Migrate::current('default');
+    }
 }
